e60c0340f3473b44ef1b8fe209ba43f0cd55a47d
S42yt <songoku42@outlook.de>
2025-01-14 20:30:00 +0300

    changes made for publish

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::process::{Command, exit};
-use std::path::Path;
+use std::env;
+use std::fs;
+use std::process::{exit, Command};
 use walkdir::WalkDir;
 
-fn format_file(file_path: &str) {
+fn format_file(file_path: &str) -> bool {
+    let original_content = fs::read_to_string(file_path).expect("Failed to read file");
+
     let status = Command::new("rustfmt")
         .arg(file_path)
         .status()
@@ -10,33 +13,43 @@ fn format_file(file_path: &str) {
 
     if !status.success() {
         eprintln!("Failed to format file: {}", file_path);
-    } else {
-        println!("Formatted: {}", file_path);
+        return false;
     }
+
+    let formatted_content = fs::read_to_string(file_path).expect("Failed to read formatted file");
+
+    if original_content != formatted_content {
+        println!("Formatted: {}", file_path);
+        return true;
+    }
+
+    false
 }
 
 fn format_all_files() {
     let current_dir = std::env::current_dir().expect("Failed to get current directory");
 
-    for entry in WalkDir::new(current_dir)
+    for entry in WalkDir::new(&current_dir)
         .into_iter()
         .filter_map(Result::ok)
         .filter(|e| e.path().extension().map(|ext| ext == "rs").unwrap_or(false))
     {
-        let file_path = entry.path().to_str().expect("Invalid file path");
-        format_file(file_path);
+        let abs_path = entry.path().canonicalize().expect("Invalid path");
+        let abs_str = abs_path.to_str().expect("Invalid file path");
+        format_file(abs_str);
     }
 }
 
 fn main() {
-    if !Command::new("rustfmt").output().is_ok() {
-        eprintln!("rustfmt is not installed or not in the PATH.");
-        exit(1);
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 && args[1] == "rusty" {
+        if !Command::new("rustfmt").output().is_ok() {
+            eprintln!("rustfmt is not installed or not in the PATH.");
+            exit(1);
+        }
+        println!("Formatting files...");
+        format_all_files();
+        println!("Done!");
+        return;
     }
-
-    println!("Starting to format Rust files...");
-
-    format_all_files();
-
-    println!("Rust file formatting completed!");
 }
